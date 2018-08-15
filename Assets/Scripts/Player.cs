bd31af94912e19bbee143a3f444c5ac3139ffaf1
DavidASC5 <achooospace@gmail.com>
2018-08-15 11:04:32 -0400

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator anim;
+    public float speed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        if (Input.GetKey(KeyCode.W))
+        { transform.Translate(0, speed * Time.deltaTime, 0); anim.SetInteger("Dir", 0); anim.speed = 1; }
+        else if (Input.GetKey(KeyCode.S))
+        { transform.Translate(0, -speed * Time.deltaTime, 0); anim.SetInteger("Dir", 1); anim.speed = 1; }
+        else if (Input.GetKey(KeyCode.A)) 
+        { transform.Translate(-speed * Time.deltaTime, 0, 0); anim.SetInteger("Dir", 2); anim.speed = 1; }
+        else if (Input.GetKey(KeyCode.D))
+        { transform.Translate(speed * Time.deltaTime, 0, 0); anim.SetInteger("Dir", 3); anim.speed = 1; }
+        else
+        {
+            anim.speed = 0;
+        }
+    }
+    void Movement()
+    {
+
+    }
+
+}
